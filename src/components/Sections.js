c45491b07bf3140ea97f0ae4d42624181ec6ec08
jurgob <jurgo.boemo@gmail.com>
2016-10-15 19:54:21 +0100

    add google analytics

--- src/components/Sections.js
+++ src/components/Sections.js
@@ -6,49 +6,69 @@ import {Layout, MenuCol, ContCol} from './Layout2Col'
 import Router from 'react-router/BrowserRouter'
 import Match from 'react-router/Match'
 
+import  ReactGA from 'react-ga';
+ReactGA.initialize(process.env.REACT_APP_JSQUEST_UA);
+
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+}
+
+const XhleeMatch = ({sections,setResponse}) => (
+  <Match
+    pattern="/*"
+    render={({ pathname, pattern }) => {
+
+      let section;
+      sections.forEach(s => {
+        const sub = s.subsections.find(sub => sub.link === pathname)
+        if(sub)
+          section = sub
+      })
+
+      if(!section)
+        return <span></span>
+
+      let introduction = "";
+      if(section.tutorial_link)
+        introduction = (<span> Read more about {section.label} here: <a href={section.tutorial_link} > xahlee.info {section.label} section</a>  </span>)
+      return (
+        <div>
+          <QuestionSection
+            {...section}
+            title={section.label}
+            introduction={introduction}
+            onCheckResponse={(id, res) => setResponse(id, res)}
+          />
+        </div>
+      )
+    }}
+   />
+)
 
 const Sections = ({sections,setResponse}) => (
   <div >
-    <Router >
-      <div>
-      <Layout>
-        <MenuCol width="280px" >
-          <Navigation />
-        </MenuCol>
-        <ContCol>
-          <Match component={StartTest} exactly pattern="/" />
-          <Match
-            pattern="/*"
-            render={({ pathname, pattern }) => {
+    {process.env.REACT_APP_JSQUEST_UA}
+    <Router
 
-              let section;
-              sections.forEach(s => {
-                const sub = s.subsections.find(sub => sub.link === pathname)
-                if(sub)
-                  section = sub
-              })
+    >
+      {() => {
+        logPageView()
+        return (
+          <div>
+          <Layout>
+            <MenuCol width="280px" >
+              <Navigation />
+            </MenuCol>
+            <ContCol>
+              <Match component={StartTest} exactly pattern="/"  />
+              <XhleeMatch {...{sections,setResponse}} />
+              </ContCol>
+            </Layout>
+          </div>
+        )
+      }}
 
-              if(!section)
-                return <span></span>
-
-              let introduction = "";
-              if(section.tutorial_link)
-                introduction = (<span> Read more about {section.label} here: <a href={section.tutorial_link} > xahlee.info {section.label} section</a>  </span>)
-              return (
-                <div>
-                  <QuestionSection
-                    {...section}
-                    title={section.label}
-                    introduction={introduction}
-                    onCheckResponse={(id, res) => setResponse(id, res)}
-                  />
-                </div>
-              )
-            }}
-           />
-          </ContCol>
-        </Layout>
-      </div>
     </Router>
   </div>
 )
