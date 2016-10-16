0ca35c214d7677ed06d680ae4729276780d4cf09
jurgob <jurgo.boemo@gmail.com>
2016-10-16 18:49:53 +0100

    add operators questions

--- src/questions/01_javascript_basics/04_operators.js
+++ src/questions/01_javascript_basics/04_operators.js
@@ -1,5 +1,57 @@
 const questions = [
-
+  {
+    code: `
+      var a;
+      console.log(a = function(){return 4})
+      log(a())
+    `
+  },
+  {
+    code: `
+      function a(b,c){ return b + c  }
+      var res = a( (b = 5), (c = 6)  );
+      log(res)
+    `
+  },
+  {
+    code: `
+      var x = (y = 5);
+      log(x);
+    `
+  },
+  {
+    code: `
+      var y = 4;
+      var x = (y += 5);
+      log(x)
+    `
+  },
+  {
+    code: `
+      var x = 1;
+      log(++x);
+    `
+  },
+  {
+    code: `
+      var x = 1;
+      ++x;
+      log(x)
+    `
+  },
+  {
+    code: `
+      var x = 1;
+      log(x++)
+    `
+  },
+  {
+    code: `
+      var x = 1;
+      x++
+      log(x)
+    `
+  }
 ]
 
 export default {
