ca2bd595c12e001c43e05492186d906ce825cef7
jurgob <jurgo.boemo@gmail.com>
2016-10-17 22:50:21 +0100

    use plain select for mobile

--- src/components/NavigationMobile.js
+++ src/components/NavigationMobile.js
@@ -6,39 +6,52 @@ import Select from 'react-select-plus';
 
 
 
-// const styles = {
-//   link:{
-//     padding:"10px",
-//     textDecoration:"none",
-//     display:"block",
-//     color:"#666"
-//   },
-//   linkActive:{
-//     color:"#000",
-//     fontWeight:"bold"
-//   }
-// }
-
-
 const NavigationMobile = ({curPath,onPathChange}) => {
-  let options = sectionsList.map(({title,subsections}) => ({
-    label: title,
-    options: subsections.map(({label,link}) => ({
-      label: label,
-      value: link
-    }))
-  }))
+
+  // let options = sectionsList.map(({title,subsections}) => ({
+  //   label: title,
+  //   options: subsections.map(({label,link}) => ({
+  //     label: label,
+  //     value: link
+  //   }))
+  // }))
+
+  const options = sectionsList.map(({title,subsections}) => (
+    <optgroup label={title} >
+      {subsections.map(({label,link}) => (
+        <option value={link} >{label}</option>
+      ))}
+    </optgroup>
+  ))
+
   return (
     <div style={{textAlign:"left"}} >
-
-      <Select
+      <select
+        style={{
+          webkitAppearance:"none",
+          borderRadius:"0",
+          background: "none",
+          border:"1px solid #ccc",
+          display:"block",
+          width:"100%",
+          padding:"5px",
+          fontSize:"15px"
+        }}
+        name="form-field-name"
+        value={curPath}
+        onChange={(e) => onPathChange(e.target.value)}
+      >
+        <option value="/" >Jump to section...</option>
+        {options}
+      </select>
+      {/* <Select
         name="form-field-name"
         options={options}
         value={curPath}
         clearable={false}
         placeholder="Jump to section..."
         onChange={(el) => onPathChange(el.value)}
-      />
+      /> */}
     </div>
   )
 }
