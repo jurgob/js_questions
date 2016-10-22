58a72fca5f3234150eb5fcd321d5050281ed5a61
jurgob <jurgo.boemo@gmail.com>
2016-10-22 14:53:08 +0100

    try catch questions

--- src/questions/01_javascript_basics/10_try_catch.js
+++ src/questions/01_javascript_basics/10_try_catch.js
@@ -1,5 +1,51 @@
 const questions = [
-
+  {
+    code: `
+      var res ="";
+      try {
+        res+="a";
+      } catch(e) {
+        res+="b";
+      }
+      finally {
+        res+="c";
+      }
+      log(res);
+    `
+  },
+  {
+    code: `
+      var res ="";
+      try {
+        res+="a";
+        throw 5
+      } catch(e) {
+        res+="b";
+      }
+      finally {
+        res+="c";
+      }
+      log(res)
+    `
+  },
+  {
+    code: `
+      try {
+        throw new Error("xyz is wrong.");
+      } catch(e) {
+        log( typeof(e)  )
+      }
+    `
+  },
+  {
+    code: `
+      try {
+        throw 5;
+      } catch(e) {
+        log( typeof(e)  )
+      }
+    `
+  }
 ]
 
 export default {
