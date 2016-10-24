4e15f4da5733627937f01462adbf9567e2937bbf
jurgob <jurgo.boemo@gmail.com>
2016-10-24 17:29:56 +0100

    call funciton as method questions

--- src/questions/02_function/07_call_function_as_method.js
+++ src/questions/02_function/07_call_function_as_method.js
@@ -1,4 +1,79 @@
+const shareCode = `
+  function ff(x) {
+    this["p"] = x;
+  };
+  var bb = {"b":2};
+  var gg = ff.bind(bb,7);
+  gg();
+`;
+
 const questions = [
+  {
+    code:`
+      function ff() {
+        return this;
+      };
+
+      var obj = {};
+
+      log( ff.call(obj) === obj );
+    `
+  },
+  {
+    code:`
+      function ff(x,y,z) {
+        this["x"] = x;
+        this["y"] = y;
+        this["z"] = z;
+      };
+
+      var oo = {};
+
+      ff.apply(oo,[7,8,9]);
+      log(oo.y)
+    `
+  },
+  {
+    code:`
+      function ff(x,y,z) {
+        this["x"] = x;
+        this["y"] = y;
+        this["z"] = z;
+      };
+
+      var oo = {};
+
+      ff.apply(oo,[7,8,9]);
+      log(oo.a)
+    `
+  },
+  {
+    code: `
+      var b = 1
+      var aa = {
+        "b": 3,
+        "ff":function () { return this;}
+      };
+
+      var bb = {"b":2};
+      var gg = aa.ff.bind(bb);
+
+      log(gg().b);
+    `
+  },
+  {
+    code:`
+      ${shareCode}
+      log(bb.b)
+    `
+  },
+  {
+    code:`
+      ${shareCode}
+      log(bb.p)
+    `
+  }
+
 ]
 
 export default {
