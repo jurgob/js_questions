42205fa7057c6bfd39778f6a8a451c6485e61e9a
jurgob <jurgo.boemo@gmail.com>
2016-10-24 17:43:35 +0100

    function questions

--- src/questions/02_function/08_function_constructor.js
+++ src/questions/02_function/08_function_constructor.js
@@ -1,4 +1,61 @@
 const questions = [
+  {
+    code: `
+      function ff() {
+        function gg() {
+          return 3;
+        }
+        return gg();
+      };
+      log(ff())
+    `
+  },
+  {
+    code: `
+      function ff() {
+        function gg() {
+          return 3;
+        }
+        return gg();
+      };
+      log(typeof gg)
+    `
+  },
+  {
+    code:`
+      var g = function f() { return 3; };
+      log(typeof g)
+    `
+  },
+  {
+    code:`
+      var g = function f() { return 3; };
+      log(typeof f)
+    `
+  },
+  {
+    code:`
+      var a = (function () { return 3; })()
+      log( a )
+    `
+  },
+  {
+    code: `
+      log((function (x) { return x; })(4))
+    `
+  },
+  {
+    code:`
+      var h1 = new Function("return 3;");
+      log(h1())
+    `
+  },
+  {
+    code:`
+      var h2 = new Function("a", "return a;");
+      log(h2(5))
+    `
+  }
 ]
 
 export default {
