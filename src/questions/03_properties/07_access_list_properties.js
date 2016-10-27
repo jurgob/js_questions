6f3bf91849ee958563fb10fec65b3a3a7722c53f
jurgob <jurgo.boemo@gmail.com>
2016-10-28 00:32:14 +0100

    access list questions

--- src/questions/03_properties/07_access_list_properties.js
+++ src/questions/03_properties/07_access_list_properties.js
@@ -1,4 +1,32 @@
 const questions = [
+  {
+    code:`
+      var o1 = {"a":3};
+
+      var o2 = Object.create(o1);
+      o2["b"] = 4;
+
+      var res = ""
+      for (var pp in o2) {
+        res+= pp + o2[pp];
+      }
+      log(res)
+    `
+  },
+  {
+    code:`
+      var obj = {"a":3, "b":4};
+      var res ="";
+
+      Object.keys(obj).forEach(
+        function (k) {
+            res += k + obj[k];
+        }
+      );
+
+      log(res)
+    `
+  }
 ]
 export default {
   label:"Access/List Properties",
