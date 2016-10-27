0a67e974be2fe0fc2633d5dc21dbed8166acecd0
jurgob <jurgo.boemo@gmail.com>
2016-10-28 00:26:06 +0100

    property existing questions

--- src/questions/03_properties/06_property_existence.js
+++ src/questions/03_properties/06_property_existence.js
@@ -1,4 +1,66 @@
+const shareCode = `
+  var o1 = {"p":1};
+  var o2 = Object.create(o1);
+`
+
 const questions = [
+  {
+    code: `
+      ${shareCode}
+
+      log(o1.hasOwnProperty("p"));
+    `
+  },
+  {
+    code: `
+      ${shareCode}
+
+      log(o2.hasOwnProperty("p"));
+    `
+  },
+  {
+    code: `
+      ${shareCode}
+
+      log(o2.hasOwnProperty("yyy"));
+    `
+  },
+  {
+    code: `
+      ${shareCode}
+
+      o2["p2"] = 2;
+
+      log("p1" in o2)
+    `
+  },
+  {
+    code: `
+      ${shareCode}
+
+      o2["p2"] = 2;
+
+      log("p2" in o2)
+    `
+  },
+  {
+    code: `
+      ${shareCode}
+
+      o2["p2"] = 2;
+
+      log("yyyyy" in o1)
+    `
+  },
+  {
+    code: `
+      ${shareCode}
+
+      o2["p2"] = 2;
+      
+      log("yyyyy" in o2)
+    `
+  },
 ]
 export default {
   label:"Check Property Existence",
