861fad7789a2ff5389f0d2f391deff2f7ae3e57c
jurgob <jurgo.boemo@gmail.com>
2016-10-28 00:09:21 +0100

    create property questions

--- src/questions/03_properties/04_create_delete_property.js
+++ src/questions/03_properties/04_create_delete_property.js
@@ -1,4 +1,25 @@
 const questions = [
+  {
+    code: `
+      var obj = {"p1":1, "p2":2};
+      obj["p3"] = 3;
+      log(obj["p3"])
+    `
+  },
+  {
+    code: `
+      var cc = {};
+      Object.defineProperty(cc, "pp", { value : 3});
+      log(cc["pp"])
+    `
+  },
+  {
+    code:`
+      var oo = {"p1":1, "p2":2};
+      delete oo["p1"]
+      log(oo["p1"])
+    `
+  }
 ]
 export default {
   label:"Create/Delete Property",
