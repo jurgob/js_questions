849ea8e25e8f6d20c9d74302fecafb7310c326b3
jurgob <jurgo.boemo@gmail.com>
2016-10-28 18:47:02 +0100

    prototype intheritance questions

--- src/questions/04_object_inheritance/03_prototype_inheritance.js
+++ src/questions/04_object_inheritance/03_prototype_inheritance.js
@@ -1,4 +1,70 @@
 const questions = [
+  {
+    code:`
+      var res = Object.getPrototypeOf(Object)   === Function.prototype;
+      log(res)
+    `
+  },
+  {
+    code:`
+      var res = Object.getPrototypeOf(Date)     === Function.prototype;
+      log(res)
+    `
+  },
+  {
+    code:`
+      var res = Object.getPrototypeOf(Date)     === Object.prototype;
+      log(res)
+    `
+  },
+  {
+    code:`
+      var res = Object.getPrototypeOf(JSON) === Object.prototype;
+      log(res)
+    `
+  },
+  {
+    code:`
+      var res = typeof Object;
+      log(res)
+    `
+  },
+  {
+    code:`
+      var obj = {};
+      log(obj.prototype)
+    `
+  },
+  {
+    code:`
+      var o1 = {};
+      var FF = function () { };
+      FF.prototype = o1;
+      var o2 = new FF();
+
+      log( Object.getPrototypeOf(o2) === o1 );
+    `
+  },
+  {
+    code:`
+      var tt = Object.create(null)
+      log(Object.getPrototypeOf(tt) === null)
+    `
+  },
+  {
+    code:`
+      var o1 = Object.create(null)
+      var o2 = {}
+      log(Object.getPrototypeOf(o1) === Object.getPrototypeOf(o2))
+    `
+  },
+  {
+    code:`
+      var o1 = {}
+      var o2 = {}
+      log(Object.getPrototypeOf(o1) === Object.getPrototypeOf(o2))
+    `
+  }
 ]
 export default {
   label:"Prototype, Inheritance",
