4656c467138a9f44950f62b29ec337d74b19aa80
jurgob <jurgo.boemo@gmail.com>
2016-10-29 16:32:01 +0100

    create_array questions

--- src/questions/05_array/02_create_array.js
+++ src/questions/05_array/02_create_array.js
@@ -1,4 +1,79 @@
 const questions = [
+  {
+    code:`
+      var x = [3, 4];
+      log(x.length)
+    `
+  },
+  {
+    code:`
+      var x = [3, 4, ];
+      log(x.length)
+    `
+  },
+  {
+    code:`
+      var x = [3, 4, ,];
+      log(x.length)
+    `
+  },
+  {
+    code:`
+      var x = ["a", ,"c"];
+      log(x[1])
+    `
+  },
+  {
+    code:`
+      var x = ["a", ,"c"];
+      log(x[2])
+    `
+  },
+  {
+    code:`
+      var x = ["a", ,"c"];
+      log(Object.getOwnPropertyNames(x).length)
+    `
+  },
+  {
+    code:`
+      var r1 = new Array(2);
+      log(r1.length)
+    `
+  },
+  {
+    code:`
+      var r1 = new Array(2);
+      log(Object.getOwnPropertyNames(r1).length)
+    `
+  },
+  {
+    code:`
+      var r2 = [undefined,undefined];
+      log(r2.length)
+    `
+  },
+  {
+    code:`
+      var r2 = [undefined,undefined];
+      log(Object.getOwnPropertyNames(r2).length)
+    `
+  },
+  {
+    code:`
+      var a1 = new Array(3);
+      a1 = a1.map(function(x){return 3;});
+      log(a1[1])
+    `
+  },
+  {
+    code:`
+      var a1 = [2,2,2];
+      a1 = a1.map(function(x){return 3;});
+      log(a1[1])
+    `
+  }
+
 ]
 export default {
   label:"Create Array",
