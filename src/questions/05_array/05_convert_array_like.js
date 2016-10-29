3a6f0d4d3887c0fbcce0e1054967566d5a633a41
jurgob <jurgo.boemo@gmail.com>
2016-10-29 18:01:10 +0100

    convert_array_like questions

--- src/questions/05_array/05_convert_array_like.js
+++ src/questions/05_array/05_convert_array_like.js
@@ -1,4 +1,107 @@
+const shareCode = `
+  var alo = {0:"a", 1:"b", 2:"c", length:3};
+  function append_z_to_string (x) {return x + "z"; }
+  var myResult = Array.prototype.map.call( alo, append_z_to_string);
+`
+
 const questions = [
+  {
+    code:`
+      var aa = {0:"a", 1:"b",2:"c", length:3};
+      log( Array.isArray(aa) )
+    `
+  },
+  {
+    code:`
+      var aa = {0:"a", 1:"b",2:"c", length:3};
+      var bb = Array.prototype.slice.call(aa);
+
+      log( Array.isArray(bb) )
+    `
+  },
+  {
+    code:`
+      log(
+        typeof Array === "function"
+      );
+    `
+  },
+  {
+    code:`
+      log(
+        Object.prototype.hasOwnProperty.call(Array,"prototype")
+      );
+    `
+  },
+  {
+    code:`
+      log(
+        Object.getPrototypeOf([]) === Array.prototype
+      );
+    `
+  },
+  {
+    code:`
+      log(
+        Object.getPrototypeOf([]) === Object.prototype
+      );
+    `
+  },
+  {
+    code:`
+      log(
+        Array.prototype.hasOwnProperty("length")
+      );
+    `
+  },
+  {
+    code:`
+      log(
+        Array.prototype.hasOwnProperty("slice")
+      );
+    `
+  },
+  {
+    code:`
+      log(
+        typeof Array.prototype.slice
+      )
+    `
+  },
+  {
+    code:`
+      log(
+        Object.getPrototypeOf(Array.prototype.slice) === Object.prototype
+      )
+    `
+  },
+  {
+    code:`
+      log(
+        Object.getPrototypeOf(Array.prototype.slice) === Function.prototype
+      )
+    `
+  },
+  {
+    code:`
+    log(
+      Function.prototype.hasOwnProperty("call") &&
+      (Object.getOwnPropertyNames(Function.prototype).indexOf("call") >= 0)
+    )
+    `
+  },
+  {
+    code:`
+      ${shareCode}
+      log( Array.isArray(myResult) )
+    `
+  },
+  {
+    code:`
+      ${shareCode}
+      log(myResult)
+    `
+  }
 ]
 export default {
   label:"Convert Array-Like Object",
