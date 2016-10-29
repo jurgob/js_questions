d64c2419565ed6914b6a58355628d0b5cc8f8fee
jurgob <jurgo.boemo@gmail.com>
2016-10-29 15:21:57 +0100

    comparison_equality_test_objects questions

--- src/questions/04_object_inheritance/11_comparison_equality_test_objects.js
+++ src/questions/04_object_inheritance/11_comparison_equality_test_objects.js
@@ -1,4 +1,43 @@
 const questions = [
+  {
+    code:`
+      var c = {"a":3};
+      var d = {"a":3};
+
+      log( c === d );
+    `
+  },
+  {
+    code:`
+      var c = {"a":3};
+      var d = {"a":3};
+
+      log( c.a === d.a );
+    `
+  },
+  {
+    code:`
+      var c = {"a":3};
+      var d = c;
+
+      log( c === d );
+    `
+  },
+  {
+    code:`
+      var x = {"a":1, "b":2};
+      var y = {"b":2, "a":1};
+      log( JSON.stringify(x) === JSON.stringify(y) )
+    `
+  },
+  {
+    code:`
+      var x = {"a":1, "b":2};
+      var y = {"a":1, "b":2};
+      log( JSON.stringify(x) === JSON.stringify(y) )
+    `
+  }
+
 ]
 export default {
   label:"Equality Test of Objects",
