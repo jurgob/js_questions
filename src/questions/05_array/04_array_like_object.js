6565a2ec7022d95fcd7de969d9e38c17ec33380e
jurgob <jurgo.boemo@gmail.com>
2016-10-29 17:50:48 +0100

    array_like_object questions

--- src/questions/05_array/04_array_like_object.js
+++ src/questions/05_array/04_array_like_object.js
@@ -1,4 +1,73 @@
+const shareCode = `
+  function ff(x,y) {
+    return arguments
+  }
+`
+
 const questions = [
+  {
+    code:`
+      var a1 = {0:"a", 1:"b" , length:2};
+      a1[2] = 99;
+      log(a1.length)
+    `
+  },
+  {
+    code:`
+      var a2 = ["a", "b"];
+      a2[2] = 99;
+      log(a2.length)
+    `
+  },
+  {
+    code:`
+      log(
+        ff("a", "b").length
+      )
+    `
+  },
+  {
+    code:`
+      log(
+        ff("a", "b")[0]
+      )
+    `
+  },
+  {
+    code:`
+      log(
+        ff("a", "b")[1]
+      )
+    `
+  },
+  {
+    code:`
+      log(
+        Array.isArray(ff("a", "b"))
+      )
+    `
+  },
+  {
+    code:`
+      log(
+        Object.getOwnPropertyNames( ff("a", "b")).length
+      )
+    `
+  },
+  {
+    code:`
+      log(
+        Object.getOwnPropertyNames( ff("a", "b"))[2]
+      )
+    `
+  },
+  {
+    code:`
+      log(
+        Object.getOwnPropertyNames( ff("a", "b"))[3]
+      )
+    `
+  },
 ]
 export default {
   label:"Array-Like Object",
