6dac280d22f751b95bed38280c71463f822dc89f
jurgob <jurgo.boemo@gmail.com>
2016-10-29 19:07:04 +0100

    object_create_emulating_constructor questions

--- src/questions/06_javascript_OOP/06_object_create_emulating_constructor.js
+++ src/questions/06_javascript_OOP/06_object_create_emulating_constructor.js
@@ -1,4 +1,40 @@
+const shareCode = `
+  var dad = {"a":3};
+  var F1 = function (x) {this.y = x;};
+  F1.prototype = dad;
+  var F2 = function (x) {
+    return Object.create(
+        dad,
+        {"y":{value: x,writable: true, enumerable: true, configurable: true}});
+  };
+
+  var o1 = new F1(4);
+  var o2 = F2(4);
+`
+
 const questions = [
+  {
+    code: `
+      ${shareCode}
+      log(
+        Object.getPrototypeOf(o1) ===
+        Object.getPrototypeOf(o2)
+      )
+
+    `,
+    solution:true
+  },
+  {
+    code: `
+      ${shareCode}
+      log(
+        JSON.stringify(Object.getOwnPropertyDescriptor(o1,"y")) ===
+        JSON.stringify(Object.getOwnPropertyDescriptor(o2,"y"))
+      )
+
+    `,
+    solution:true
+  },
 ]
 export default {
   label:`Object.create as Constructor`,
