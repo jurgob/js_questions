9188eae9b9cc206043530ad3d634524412514524
jurgob <jurgo.boemo@gmail.com>
2016-10-29 14:25:05 +0100

    find_objects_prototype questions

--- src/questions/04_object_inheritance/04_find_objects_prototype.js
+++ src/questions/04_object_inheritance/04_find_objects_prototype.js
@@ -1,4 +1,76 @@
+const shareCode = `
+
+  var t1 = {};
+  var t2 = Object.create(t1);
+  var t3 = Object.create(t2);
+  var t4 = Object.create(t3);
+
+`
+
 const questions = [
+  {
+    code:`
+      var myArray = [3,4];
+      log(
+        Object.getPrototypeOf(myArray) === myArray.__proto__
+      );
+    `
+  },
+  {
+    code:`
+      var myArray = [3,4];
+      log(
+        Object.getPrototypeOf(myArray) === myArray.prototype
+      );
+    `
+  },
+  {
+    code:`
+      var myArray = [3,4];
+      log(
+        Object.getPrototypeOf(myArray) === Array.prototype
+      );
+    `
+  },
+  {
+    code:`
+      ${shareCode}
+      log( t1.isPrototypeOf(t4) )
+    `
+  },
+  {
+    code:`
+      ${shareCode}
+      log( t1.isPrototypeOf(t1) )
+    `
+  },
+  {
+    code:`
+      ${shareCode}
+      log( t3.isPrototypeOf(t1) )
+    `
+  },
+  {
+    code:`
+      ${shareCode}
+      log( Object.prototype.isPrototypeOf(t1) )
+    `
+  },
+  {
+    code:`
+      ${shareCode}
+      var obj = {}
+      log( obj.isPrototypeOf(t1) )
+    `
+  },
+  ,
+  {
+    code:`
+      ${shareCode}
+      var obj = {}
+      log( obj.__proto__.isPrototypeOf(t1) )
+    `
+  }
 ]
 export default {
   label:"Find Object's Parent",
