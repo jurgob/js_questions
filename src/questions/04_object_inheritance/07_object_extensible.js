c1e3801794cd890e6b3799f7067d9bac052d47a6
jurgob <jurgo.boemo@gmail.com>
2016-10-29 15:08:37 +0100

    object_extensible responses

--- src/questions/04_object_inheritance/07_object_extensible.js
+++ src/questions/04_object_inheritance/07_object_extensible.js
@@ -1,4 +1,119 @@
+const shareCode = `
+  var uu = {};
+  Object.preventExtensions(uu);
+`
 const questions = [
+  {
+    code:`
+      log(Object.isExtensible(uu))
+    `
+  },
+  {
+    code:`
+      uu.pp = 3;
+      log(uu.hasOwnProperty("pp"))
+    `
+  },
+  {
+    code:`
+      var t = {"p":3};
+      Object.preventExtensions(t);
+      delete t.p;
+      log(t.p)
+    `
+  },
+  {
+    code:`
+      var oo = {"p1":1, "p2":2};
+      log(
+        Object.getOwnPropertyDescriptor(oo, "p1").configurable
+      )
+    `
+  },
+  {
+    code:`
+      var oo = {"p1":1, "p2":2};
+      log(
+        Object.getOwnPropertyDescriptor(oo, "p1").writable
+      )
+    `
+  },
+  {
+    code:`
+      var oo = {"p1":1, "p2":2};
+      Object.seal(oo);
+      log(
+        Object.getOwnPropertyDescriptor(oo, "p1").configurable
+      )
+    `
+  },
+  {
+    code:`
+      var oo = {"p1":1, "p2":2};
+      Object.seal(oo);
+      log(
+        Object.getOwnPropertyDescriptor(oo, "p1").writable
+      )
+    `
+  },
+  {
+    code:`
+      var oo = {"p1":1, "p2":2};
+      Object.seal(oo);
+      log(
+        Object.isExtensible(oo
+      )
+    `
+  },
+  {
+    code:`
+      var oo = {"p1":1, "p2":2};
+      log(Object.getOwnPropertyDescriptor(oo, "p1").writable )
+    `
+  },
+  {
+    code:`
+      var oo = {"p1":1, "p2":2};
+      log(Object.getOwnPropertyDescriptor(oo, "p1").configurable )
+    `
+  },
+  {
+    code:`
+      var oo = {"p1":1, "p2":2};
+      Object.freeze(oo);
+      log(Object.getOwnPropertyDescriptor(oo, "p1").writable )
+    `
+  },
+  {
+    code:`
+      var oo = {"p1":1, "p2":2};
+      Object.freeze(oo);
+      log(Object.getOwnPropertyDescriptor(oo, "p1").configurable )
+    `
+  },
+  {
+    code:`
+      var oo = {"p1":1, "p2":2};
+      Object.freeze(oo);
+      log(Object.isExtensible(oo) )
+    `
+  },
+  {
+    code:`
+      var oo = {};
+      log(Object.isFrozen(oo));
+    `
+  },
+  {
+    code:`
+      var oo = {};
+      Object.freeze(oo);
+      log(Object.isFrozen(oo));
+    `
+  }
+
+
+
 ]
 export default {
   label:"Prevent Modification of Object",
