550448b27f375f1a9d20b3574639cc358a7356bd
jurgob <jurgo.boemo@gmail.com>
2016-10-29 19:03:06 +0100

    instanceof questions

--- src/questions/06_javascript_OOP/05_instanceof.js
+++ src/questions/06_javascript_OOP/05_instanceof.js
@@ -1,4 +1,72 @@
 const questions = [
+  {
+    code:`
+      function FF () {
+        this.p = 3;
+      };
+
+      var f1 = new FF();
+      log( f1 instanceof FF);
+    `
+  },
+  {
+    code:`
+      function ff () {};
+      var x = {};
+
+      ff.prototype = Object.prototype;
+
+      log( x instanceof ff)
+    `
+  },
+  {
+    code:`
+      function ff () {};
+      var x = {};
+
+      ff.prototype = Object.prototype;
+
+      log( ff.prototype.isPrototypeOf(x) )
+    `
+  },
+  {
+    code:`
+      function FF () {return {};}; // returns a object
+      var f1 = new FF();
+
+      log( f1 instanceof FF)
+    `
+  },
+  {
+    code:`
+      function FF () {return {};}; // returns a object
+      var f1 = new FF();
+
+      log( FF.prototype.isPrototypeOf(f1) )
+    `
+  },
+  {
+    code:`
+      function FF () {
+        this.p = 3;
+      };
+      var f1 = new FF();
+      FF.prototype = [];
+
+      log( f1 instanceof FF);
+    `
+  },
+  {
+    code:`
+      function FF () {
+        this.p = 3;
+      };
+      var f1 = new FF();
+      FF.prototype = [];
+
+      log( FF.prototype.isPrototypeOf(f1) );
+    `
+  }
 ]
 export default {
   label:`Operator "instanceof"`,
