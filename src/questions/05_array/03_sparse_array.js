c8c61c59e4f81fa33091fb64a0c582de4b08ef4e
jurgob <jurgo.boemo@gmail.com>
2016-10-29 17:44:26 +0100

    sparse_array questions

--- src/questions/05_array/03_sparse_array.js
+++ src/questions/05_array/03_sparse_array.js
@@ -1,4 +1,42 @@
+const shareCode = `
+  var cc = [];
+  cc[2] = "b";
+`
 const questions = [
+  {
+    code:`
+      var rr = ["a", "b"];
+      delete rr[1];
+      log(rr.length)
+    `
+  },
+  {
+    code:`
+      var rr = ["a", "b"];
+      delete rr[1];
+      log(Object.keys(rr))
+    `
+  },
+  {
+    code:`
+      ${shareCode}
+      log(cc[0])
+    `
+  },
+  {
+    code:`
+      ${shareCode}
+      log(cc[2])
+    `
+  },
+  {
+    code:`
+      ${shareCode}
+      log(cc.length)
+    `
+  }
+
+
 ]
 export default {
   label:"Sparse Array",
