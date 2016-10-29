ddd81a022e1b10e3fd653bbaf92f55523a18790b
jurgob <jurgo.boemo@gmail.com>
2016-10-29 15:18:00 +0100

    clone_object questions

--- src/questions/04_object_inheritance/10_clone_object.js
+++ src/questions/04_object_inheritance/10_clone_object.js
@@ -1,4 +1,20 @@
 const questions = [
+  {
+    code:`
+      var obj1 = {a:1}
+      var obj2 = obj1;
+      obj2.a =2;
+      log(obj1.a)
+    `
+  },
+  {
+    code:`
+      var obj1 = {a:1}
+      var obj2 = JSON.parse(JSON.stringify(obj1));
+      obj2.a =2;
+      log(obj1.a)
+    `
+  },
 ]
 export default {
   label:"Clone Object",
